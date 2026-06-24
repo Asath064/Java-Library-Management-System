56182e0d1ab0aa1755f688eb321c84750b3615b7
Asath Ali <143001521+Asath064@users.noreply.github.com>
2026-06-24 20:12:36 +0530

    Refactor LibraryManagementSystem for simplified options

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -1,55 +1,43 @@
-import java.sql.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class LibraryManagementSystem {
 
-    static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> books = new ArrayList<>();
 
         int choice;
 
         do {
-
             System.out.println("\n===== LIBRARY MANAGEMENT SYSTEM =====");
             System.out.println("1. Add Book");
             System.out.println("2. View Books");
-            System.out.println("3. Update Book");
-            System.out.println("4. Delete Book");
-            System.out.println("5. Borrow Book");
-            System.out.println("6. Return Book");
-            System.out.println("7. Exit");
-
+            System.out.println("3. Delete Book");
+            System.out.println("4. Exit");
             System.out.print("Enter Choice: ");
             choice = sc.nextInt();
+            sc.nextLine();
 
             switch(choice) {
 
                 case 1:
-                    addBook();
+                    System.out.print("Enter Book Name: ");
+                    books.add(sc.nextLine());
+                    System.out.println("Book Added!");
                     break;
 
                 case 2:
-                    viewBooks();
+                    System.out.println("Books: " + books);
                     break;
 
                 case 3:
-                    updateBook();
+                    System.out.print("Enter Book Name: ");
+                    books.remove(sc.nextLine());
+                    System.out.println("Book Deleted!");
                     break;
 
                 case 4:
-                    deleteBook();
-                    break;
-
-                case 5:
-                    borrowBook();
-                    break;
-
-                case 6:
-                    returnBook();
-                    break;
-
-                case 7:
                     System.out.println("Thank You!");
                     break;
 
@@ -57,173 +45,8 @@ public class LibraryManagementSystem {
                     System.out.println("Invalid Choice!");
             }
 
-        } while(choice != 7);
-    }
+        } while(choice != 4);
 
-    static void addBook() {
-
-        try(Connection con =
-                    DBConnection.getConnection()) {
-
-            sc.nextLine();
-
-            System.out.print("Enter Title: ");
-            String title = sc.nextLine();
-
-            System.out.print("Enter Author: ");
-            String author = sc.nextLine();
-
-            String sql =
-                    "INSERT INTO books(title,author,status) VALUES(?,?,?)";
-
-            PreparedStatement ps =
-                    con.prepareStatement(sql);
-
-            ps.setString(1, title);
-            ps.setString(2, author);
-            ps.setString(3, "Available");
-
-            ps.executeUpdate();
-
-            System.out.println("Book Added Successfully!");
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void viewBooks() {
-
-        try(Connection con =
-                    DBConnection.getConnection()) {
-
-            Statement st =
-                    con.createStatement();
-
-            ResultSet rs =
-                    st.executeQuery("SELECT * FROM books");
-
-            while(rs.next()) {
-
-                System.out.println(
-                        rs.getInt("id")
-                        + " | "
-                        + rs.getString("title")
-                        + " | "
-                        + rs.getString("author")
-                        + " | "
-                        + rs.getString("status")
-                );
-            }
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void updateBook() {
-
-        try(Connection con =
-                    DBConnection.getConnection()) {
-
-            System.out.print("Book ID: ");
-            int id = sc.nextInt();
-
-            sc.nextLine();
-
-            System.out.print("New Title: ");
-            String title = sc.nextLine();
-
-            String sql =
-                    "UPDATE books SET title=? WHERE id=?";
-
-            PreparedStatement ps =
-                    con.prepareStatement(sql);
-
-            ps.setString(1, title);
-            ps.setInt(2, id);
-
-            ps.executeUpdate();
-
-            System.out.println("Book Updated!");
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void deleteBook() {
-
-        try(Connection con =
-                    DBConnection.getConnection()) {
-
-            System.out.print("Book ID: ");
-            int id = sc.nextInt();
-
-            String sql =
-                    "DELETE FROM books WHERE id=?";
-
-            PreparedStatement ps =
-                    con.prepareStatement(sql);
-
-            ps.setInt(1, id);
-
-            ps.executeUpdate();
-
-            System.out.println("Book Deleted!");
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void borrowBook() {
-
-        try(Connection con =
-                    DBConnection.getConnection()) {
-
-            System.out.print("Book ID: ");
-            int id = sc.nextInt();
-
-            String sql =
-                    "UPDATE books SET status='Borrowed' WHERE id=?";
-
-            PreparedStatement ps =
-                    con.prepareStatement(sql);
-
-            ps.setInt(1, id);
-
-            ps.executeUpdate();
-
-            System.out.println("Book Borrowed!");
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void returnBook() {
-
-        try(Connection con =
-                    DBConnection.getConnection()) {
-
-            System.out.print("Book ID: ");
-            int id = sc.nextInt();
-
-            String sql =
-                    "UPDATE books SET status='Available' WHERE id=?";
-
-            PreparedStatement ps =
-                    con.prepareStatement(sql);
-
-            ps.setInt(1, id);
-
-            ps.executeUpdate();
-
-            System.out.println("Book Returned!");
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        sc.close();
     }
 }
